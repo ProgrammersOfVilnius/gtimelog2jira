1e4627fe39e4b4d6fed1dd9f2b76d2eb3865332f
Marius Gedminas <marius@gedmin.as>
2020-04-21 11:21:08 +0300

    Add Python 3.8 to test matrix

--- setup.py
+++ setup.py
@@ -20,4 +20,10 @@ setup(
             'gtimelog2jira=gtimelog2jira:main',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires='>= 3.6',
 )
